f72e62fcb9024d071fee9e3e93daaf21c9c9e0f2
Cimbali <me@cimba.li>
2023-03-28 16:14:59 +0100

    Add README and update script

---
+++ README.md
@@ -0,0 +1,3 @@
+A repo containing spec files to build [Mozilla VPN](https://github.com/mozilla-mobile/mozilla-vpn-client) on OpenSUSE with
+- minimal diff-changes with respect to upstream Fedora-like spec files
+- a single `spec-cleaner` away of generating OpenSUSE-compliant spec files
---
+++ update.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# Parameters
+repo=mozilla-mobile/mozilla-vpn-client
+workflow=Sources
+releases=releases.json
+build=build
+
+# Curl common args to access the GH API as an array
+curl=(curl -s -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28")
+# You may skip tokens, or use one from env, etc.
+curl+=(-H "Authorization: Bearer `kwallet-query -r cimbali@github kdewallet`")
+
+# Fetch/get tag
+if (( $# )); then
+	tag=$1
+	shift
+
+	if [[ "$tag" =~ ^v[0-9]+(\.[0-9]+){2}$ ]]; then
+		echo Using tag $tag
+	else
+		echo "Tag $tag does not fit the expected format 'v{Major}.{Minor}.{Patch}'"
+		exit 1
+	fi
+else
+	"${curl[@]}" "https://api.github.com/repos/$repo/releases" -o "$build/$releases"
+	tag=`jq -r '.[0].tag_name' "$build/$releases"`
+	echo Latest tag fetched: $tag
+fi
+
+
+# Ensure we have the right files
+orig=mozillavpn_${tag#v}.orig.tar.gz
+pack=Sources_${tag#v}.zip
+spec=mozillavpn.spec
+changes=mozillavpn.changes
+
+mkdir -p "$build/"
+
+if [ ! -f "$build/$pack" ]; then
+	for (( page=1 ; page < 10 ; ++page )); do
+		rel=`"${curl[@]}" "https://api.github.com/repos/$repo/actions/artifacts?name=$workflow&per_page=100&page=$page" |
+			jq -r ".artifacts[] | select(.workflow_run.head_branch == \"$tag\")"`
+		[ -n "$rel" ] && break
+	done
+
+	url=`echo "$rel" | jq -r '.archive_download_url'`
+	# Get Location: header, watch out for CRLF end lines
+	dl=`"${curl[@]}" -D - "$url" | awk -F '[ \n\r\t]' 'tolower($1) == "location:" { print $2 }'`
+
+	# non-silent curl
+	curl "$dl" -o "$build/$pack"
+fi
+
+if [ ! -f "$build/$orig" ]; then
+	unzip -o "$build/$pack" "$orig" && mv "$orig" "$build/"
+fi
+
+unzip -o "$build/$pack" "$spec"
+
+ls -l "$build/$orig" "$spec"
+
+# Bail if anything goes wrong
+set -e
+
+# Manual steps
+# import spec file updates
+vim "$spec" +Gdiff
+
+(
+# Prepend version item to changelog
+echo "- Update to version $tag"
+# Also prepend pandoc-wrapped (for list awareness) body of github release
+jq -r '.[0].body' "$build/$releases" | sed -r \
+	-e 's/^[-*]/  &/;s/\r$//;/What.s New|Full Changelog|Downloads at/d;/New Contributors/,/^$/d' \
+	-e 's,(#|https://github.com/mozilla-mobile/mozilla-vpn-client/pull/)([0-9]+),gh#\2,'
+) | pandoc --from=markdown --to=markdown --columns=70 |
+	sed -r 's/^ {8}/    /;s/^- {3}/- /;s/^ {4}- {3}/  * /;$a\\' |
+	sed -i '0r/dev/stdin' "$changes"
+
+# Update changelog
+osc vc
+
+# Build source rpm
+spec-cleaner "$spec" > "$build/$spec"
+
+absbuild="`readlink -f "$build"`"
+rpmbuild -D "_topdir $absbuild" -D "_sourcedir $absbuild" -D "_srcrpmdir $absbuild" -bs "$build/$spec"
+
+git commit -em "Version $tag" "$spec" "$changes"
